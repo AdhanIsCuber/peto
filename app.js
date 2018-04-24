0332b4be4b3fbba920975e6d1a85449b10e737ac
Wirya Ramadhan <adhaniscuber@gmail.com>
2018-04-24 15:29:26 +0700

    setup signup function

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
 const hbs = require('hbs');
-const login = require('./app/login');
-const helps = require('./app/helps');
-const account = require('./app/account');
+
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const mlabPetoDB = process.env.MLAB_DATABASE_PETO;
+
+// configuration connect DB
+mongoose.connect(mlabPetoDB);
+require('./config/passport')(passport);
+
+// route
+const login = require('./app/login')(app, passport);
+const helps = require('./app/helps')(app, passport);
+const account = require('./app/account')(app, passport);
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+
+// required for passport
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,18 +51,20 @@ hbs.registerPartials(__dirname + '/views/base');
 hbs.registerPartials(__dirname + '/views/components');
 hbs.registerPartials(__dirname + '/views/dummy');
 
+
 app.use('/login', login);
 app.use('/helps', helps);
 app.use('/account', account);
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    layout: './layouts/default'
-  });
-});
 
 app.listen(8000, (req, res) => {
   console.log('server peto on http://127.0.0.1:8000')
 })
 
-module.exports = app;
+module.exports = (app, passport) => {
+  app.get('/', (req, res) => {
+    res.render('index', {
+      layout: './layouts/default'
+    });
+  });
+}
--- app/login.js
+++ app/login.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const login = express.Router();
-
-login.get('/', (req, res) => {
-  res.render('login', {
-    layout: './layouts/default'
+module.exports = (app, passport) => {
+  app.get('/', (req, res) => {
+    res.render('login', {
+      layout: './layouts/default'
+    })
   })
-})
 
+  app.post('/login', (req, res) => {
+    console.log('log')
+  })
 
-module.exports = login;
+  app.post('signup', passport.authenticate('local-signup', {
+    successRedirect: '/', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+  })
+};
+
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/login');
+}
